4d12a3a8bb954e16db601f6dcedf85b37d92d937
Mike Starina <x@pnhd.ru>
2023-06-01 15:01:15 +0300

    controllers fixed

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -11,11 +11,13 @@ import ServerError from '../utils/server-error-class';
 
 export const createOrder = async (req: Request, res: Response, next: NextFunction) => {
 
-  const { owner_phone, to, from } = await req.body;
+  //console.log(req.body);
 
+  const { owner_phone, to, from } = await req.body.data;
+  console.log(to);
   try {
 
-    if (!owner_phone || !to || !from) throw ServerError.error400('Некорректные данные в запросе')
+    /*if (!owner_phone || !to || !from) throw ServerError.error400('Некорректные данные в запросе')*/
 
     const newOrder = await new order({ owner_phone, to, from });
 
