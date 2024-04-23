be32aff8ef5b20a10167394b7d2bf10757c46375
Mike Starina <x@pnhd.ru>
2024-04-23 15:39:16 +0300

    leads contr updated

--- src/controllers/lead.ts
+++ src/controllers/lead.ts
@@ -10,17 +10,16 @@ import { sendMail } from "../utils/mailer";
 
 export const createLead = async (req: Request, res: Response, next: NextFunction) => {
 
-  const { name, phone } = await req.body;
-
+  const { name, phone, from, to } = await req.body;
 
 
   try {
 
     //if (!name || !phone) throw ServerError.error400('Некорректные данные в запросе')
-    const newLead = new lead({ name, phone });
+    const newLead = new lead({ name, phone, from, to});
     if (!newLead) throw ServerError.error500('Создать новый лид в базе не удалось')
 
-    const payload = `Имя: ${name}, Телефон: ${phone}`;
+    const payload = `Имя: ${name ? name : ''}, Телефон: ${phone}, Откуда: ${from ? from : ''}, Куда: ${to ? to : ''}`;
     await sendMail({to: 'info@outlook-logistics.ru', subject: 'Новая заявка на звонок', payload})
 
     await newLead.save()
